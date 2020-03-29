fe27458a0f75d9fe7ea2750fed06a35d27284662
Peter Delevoryas <me@peterdelevoryas.com>
2020-03-29 16:46:42 -0700

    Fixup build.rs so r1 compiles
    For some reason, zlib is needed now.

--- build.rs
+++ build.rs
@@ -4,22 +4,22 @@ use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
 fn link_lib(name: &str) {
-	println!("cargo:rustc-link-lib={}", name);
+    println!("cargo:rustc-link-lib={}", name);
 }
 
 fn link_search(path: &str) {
-	println!("cargo:rustc-link-search={}", path);
+    println!("cargo:rustc-link-search={}", path);
 }
 
 fn output(c: &mut Command) -> String {
-	let output = match c.stderr(Stdio::inherit()).output() {
-		Ok(s) => s,
-		Err(e) => panic!("failed to execute {:?}: {}", c, e),
-	};
-	if !output.status.success() {
-		panic!("command exited with error: {:?}: {}", c, output.status);
-	}
-	String::from_utf8(output.stdout).unwrap()
+    let output = match c.stderr(Stdio::inherit()).output() {
+        Ok(s) => s,
+        Err(e) => panic!("failed to execute {:?}: {}", c, e),
+    };
+    if !output.status.success() {
+        panic!("command exited with error: {:?}: {}", c, output.status);
+    }
+    String::from_utf8(output.stdout).unwrap()
 }
 
 fn main() {
@@ -33,23 +33,27 @@ fn main() {
         .write_to_file(&out_file)
         .expect(&format!("unable to writing bindings to {}", out_file.display()));
 
-	let mut cmd = Command::new("llvm-config");
-	cmd.arg("--cxxflags")
-		.arg("--ldflags")
-		.arg("--system-libs")
-		.arg("--libs")
-		.arg("x86");
+    let mut cmd = Command::new("llvm-config");
+    cmd.arg("--libs")
+        .arg("core")
+        .arg("x86codegen")
+        .arg("--system-libs");
 
-	for arg in output(&mut cmd).split_whitespace() {
-		if arg.starts_with("-l") {
-			link_lib(&arg[2..]);
-			continue
-		}
+    let llvm_config_output = output(&mut cmd);
+    println!("llvm-config output: {}", llvm_config_output);
 
-		if arg.starts_with("-L") {
-			link_search(&arg[2..]);
-		}
-	}
+    for arg in llvm_config_output.split_whitespace() {
+        if arg.starts_with("-l") {
+            link_lib(&arg[2..]);
+            continue
+        }
 
-	link_lib("c++");
+        if arg.starts_with("-L") {
+            link_search(&arg[2..]);
+        }
+    }
+
+    link_lib("c");
+    link_lib("c++");
+    link_lib("z");
 }
